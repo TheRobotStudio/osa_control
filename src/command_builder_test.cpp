cab89793ae64b9c5b49c2b0b31a84fe2ec3b7f5c
Cyril Jourdan <cyril.jourdan@therobotstudio.com>
2019-12-14 15:39:15 +0100

    update v0.1.1

--- src/command_builder_test.cpp
+++ src/command_builder_test.cpp
@@ -1,16 +1,19 @@
 /*
- * Copyright (c) 2018, The Robot Studio
- *  All rights reserved.
+ * Copyright (c) 2019, The Robot Studio
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *	* Redistributions of source code must retain the above copyright notice, this
- *	  list of conditions and the following disclaimer.
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
  *
- *	* Redistributions in binary form must reproduce the above copyright notice,
- *	  this list of conditions and the following disclaimer in the documentation
- *	  and/or other materials provided with the distribution.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -26,7 +29,7 @@
 
 /**
  * @file command_builder.cpp
- * @author Cyril Jourdan <cyril.jourdan@therobotstudio.com>
+ * @author Cyril Jourdan <contact@therobotstudio.com>
  * @date Modified on Apr 18, 2018
  * @date Created on Mar 15, 2013
  * @version 0.1.1
